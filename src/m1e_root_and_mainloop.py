88cc617a01a246c8eb42d8006b95dc22b4513019
liuh8 <liuh8@rose-hulman.edu>
2019-01-31 14:51:35 -0500

    24-m5

--- src/m1e_root_and_mainloop.py
+++ src/m1e_root_and_mainloop.py
@@ -7,8 +7,8 @@ A window should pop up.  That's all for this demo.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hongyu Liu.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk  # Necessary in all but this trivial example.
@@ -18,7 +18,7 @@ def main():
     root = tkinter.Tk()
     root.mainloop()
 
-    print('Done with the Event Loop')  # Note when this line runs.
+    print('Done with the Event Loop')  # Note when this line runs. (after window was closed)
 
 
 # -----------------------------------------------------------------------------
--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,8 +3,8 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hongyu Liu.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -13,26 +13,29 @@ from tkinter import ttk
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # done: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    window = tkinter.Tk()
 
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # done: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
-
+    frame = ttk.Frame(window, padding=20)
+    frame.grid()
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # done: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
-
+    hello_button = ttk.Button(frame, text='Hello')
+    hello_button.grid()
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # done: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
-
+    hello_button['command'] = (lambda: print('hello'))
     # -------------------------------------------------------------------------
     # TODO: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
@@ -41,7 +44,21 @@ def main():
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    entry_box = ttk.Entry(frame)
+    entry_box.grid()
 
+    second_button = ttk.Button(frame, text='hello or goodbye?')
+    second_button['command'] = (lambda:print_things(entry_box))
+    second_button.grid()
+
+    window.mainloop()
+
+def print_things(box):
+    contents_of_entry = box.get()
+    if contents_of_entry == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
     # -------------------------------------------------------------------------
     # TODO: 7.
     #    -- Put a second Entry on the Frame.
